2076cb016da68a5d492ae686a074cdd5d1ea4e41
Tran <94294434+s1910769124@users.noreply.github.com>
2021-11-14 13:19:24 +0100

    Create subtask8.1.py

---
+++ subtask8.1.py
@@ -0,0 +1,13 @@
+# Trantran
+p = 0
+n = int(input("please input number:"))
+
+while (p*p) < n:
+    p = p + 1
+
+if (p*p) == n:
+    print (p*p)
+else:
+    p = p-1
+    print (p*p)
+   '# it looks like I learned how to use git today 
